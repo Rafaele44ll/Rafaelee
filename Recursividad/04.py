e5dcfd7fd217b6dc4842a379b479cf66dc2d230f
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-15 22:44:57 -0500

    py

--- Recursividad/04.py
+++ Recursividad/04.py
@@ -1,2 +1,10 @@
 import os
 os.system("cls")
+
+def esta_ordenada(lista):
+    if len(lista) <= 1:
+        return True
+    return lista[0] <= lista[1] and esta_ordenada(lista[1:])
+
+resultado = esta_ordenada([1, 2, 3, 4, 5])
+print(resultado)  
