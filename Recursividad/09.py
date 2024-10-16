8d100385b69025364dbc0d3f927c3319368f9911
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-16 14:56:12 -0500

    py

--- Recursividad/09.py
+++ Recursividad/09.py
@@ -1,2 +1,14 @@
 import os
 os.system("cls")
+
+def es_palindromo(cadena):
+    cadena = cadena.lower()  
+    if len(cadena) <= 1:
+        return True
+    if cadena[0] != cadena[-1]:
+        return False
+    return es_palindromo(cadena[1:-1])
+
+print(es_palindromo("Anita lava la tina"))  
+print(es_palindromo("Hola"))                 
+
