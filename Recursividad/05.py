ce3be194359552a416b8f82ce33ff35b7930d17c
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-15 23:17:57 -0500

    py

--- Recursividad/05.py
+++ Recursividad/05.py
@@ -1,2 +1,11 @@
 import os
 os.system("cls")
+
+def suma_diccionario(dicc):
+    if not dicc:
+        return 0
+    clave, valor = dicc.popitem()
+    return valor + suma_diccionario(dicc)
+
+resultado = suma_diccionario({'a': 1, 'b': 2, 'c': 3})
+print(resultado)  
